ff87d6e66f969e06875d55969be550905078db99
shaswat.kumar <shaswat.kumar@servicenow.com>
2022-09-04 01:03:25 +0530

    Added Logic to insert Player's piece at appropritae location & adding the WIN logic checker function

--- Connect-4 Game/index.js
+++ Connect-4 Game/index.js
@@ -1,18 +1,163 @@
 let currentPlayer = 1;
 
 function createBoard() {
-    const grid = document.getElementById("grid");
+    const gridDiv = document.getElementById("grid");
 
     for (let i = 0; i < 49; i++) {
-        if (i < 42) {
-            const square = document.createElement("div");
-            grid.appendChild(square);
+        const squareNewDiv = document.createElement("div");
+        squareNewDiv.setAttribute("square-id", i);
+        squareNewDiv.addEventListener("click", addPlayerPiece);
+
+        if (i >= 42) {
+            squareNewDiv.className = "taken";
         }
-        else {
-            const square = document.createElement("div");
-            square.className = "taken";
-            grid.appendChild(square);
-        }
+
+        gridDiv.appendChild(squareNewDiv);
     }
 }
 createBoard();
+
+const squaresDiv = document.querySelectorAll("#grid div");
+
+function addPlayerPiece(e) {
+    let result = null;
+    let squareId = parseInt(e.srcElement.getAttribute("square-id"));
+
+    const currentPlayerSpan = document.getElementById("current_player");
+
+    if (squaresDiv[squareId + 7].classList.contains("taken")) {
+        squaresDiv[squareId].classList.add("taken");
+
+        if (currentPlayer === 1) {
+            squaresDiv[squareId].classList.add("player_one");
+        }
+        else {
+            squaresDiv[squareId].classList.add("player_two");
+        }
+
+        result = gameWinLogic(squareId, currentPlayer);
+
+        squaresDiv[squareId].removeEventListener("click", addPlayerPiece);
+
+        if (result === false) {
+            currentPlayer = currentPlayer === 1 ? 2 : 1;
+            currentPlayerSpan.innerText = currentPlayer;
+        }
+        else {
+            document.getElementById("result").innerText = `Player ${currentPlayer} WINS!!`;
+        }
+    }
+    else {
+        console.warn("Can't put here");
+    }
+}
+
+function gameWinLogic(squareId, player) {
+    // ! - Check for Player 1, if any of the logic is satisifed or not
+    if (player === 1) {
+        const clasListToCheck = ["taken", "player_one"];
+
+        //  ! - Check Downward direction
+        if (
+            squareId < 21 &&
+            clasListToCheck.every(e => squaresDiv[squareId + 7].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 14].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 21].classList.contains(e))
+        ) {
+            return "1";
+        }
+        // ! - Check Down-Right direction
+        else if (
+            ((squareId >= 0 && squareId < 4) || (squareId >= 7 && squareId < 11) || (squareId >= 14 && squareId < 18)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 8].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 16].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 24].classList.contains(e))
+        ) {
+            return "2";
+        }
+        // ! - Check Down-Left direction
+        else if (
+            ((squareId >= 3 && squareId < 7) || (squareId >= 10 && squareId < 14) || (squareId >= 17 && squareId < 21)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 6].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 12].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 18].classList.contains(e))
+        ) {
+            return "3";
+        }
+        // ! - Check Right direction
+        else if (
+            ((squareId >= 0 && squareId < 4) || (squareId >= 7 && squareId < 11) || (squareId >= 14 && squareId < 18) ||
+                (squareId >= 21 && squareId < 25) || (squareId >= 28 && squareId < 31) || (squareId >= 35 && squareId < 39)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 1].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 2].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 3].classList.contains(e))
+        ) {
+            return "4";
+        }
+        // ! - Check Left direction
+        else if (
+            ((squareId >= 3 && squareId < 7) || (squareId >= 10 && squareId < 14) || (squareId >= 17 && squareId < 21) ||
+                (squareId >= 24 && squareId < 28) || (squareId >= 31 && squareId < 35) || (squareId >= 38 && squareId < 42)) &&
+            clasListToCheck.every(e => squaresDiv[squareId - 1].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId - 2].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId - 3].classList.contains(e))
+        ) {
+            return "5";
+        }
+    }
+
+    // ! - Check for Player 2, if any of the logic is satisifed or not
+    else if (player === 2) {
+        const clasListToCheck = ["taken", "player_two"];
+
+        //  ! - Check Downward direction
+        if (
+            squareId < 21 &&
+            clasListToCheck.every(e => squaresDiv[squareId + 7].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 14].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 21].classList.contains(e))
+        ) {
+            return "1";
+        }
+        // ! - Check Down-Right direction
+        else if (
+            ((squareId >= 0 && squareId < 4) || (squareId >= 7 && squareId < 11) || (squareId >= 14 && squareId < 18)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 8].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 16].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 24].classList.contains(e))
+        ) {
+            return "2";
+        }
+        // ! - Check Down-Left direction
+        else if (
+            ((squareId >= 3 && squareId < 7) || (squareId >= 10 && squareId < 14) || (squareId >= 17 && squareId < 21)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 6].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 12].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 18].classList.contains(e))
+        ) {
+            return "3";
+        }
+        // ! - Check Right direction
+        else if (
+            ((squareId >= 0 && squareId < 4) || (squareId >= 7 && squareId < 11) || (squareId >= 14 && squareId < 18) ||
+                (squareId >= 21 && squareId < 25) || (squareId >= 28 && squareId < 31) || (squareId >= 35 && squareId < 39)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 1].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 2].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId + 3].classList.contains(e))
+        ) {
+            return "4";
+        }
+        // ! - Check Left direction
+        else if (
+            ((squareId >= 3 && squareId < 7) || (squareId >= 10 && squareId < 14) || (squareId >= 17 && squareId < 21) ||
+                (squareId >= 24 && squareId < 28) || (squareId >= 31 && squareId < 35) || (squareId >= 38 && squareId < 42)) &&
+            clasListToCheck.every(e => squaresDiv[squareId - 1].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId - 2].classList.contains(e)) &&
+            clasListToCheck.every(e => squaresDiv[squareId - 3].classList.contains(e))
+        ) {
+            return "5";
+        }
+    }
+
+    return false;
+}
